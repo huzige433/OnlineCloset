0d2a4aa6331ea65b216f81f821c27e5be8c4d189
胡峰 <hufeng@runxingsc.cn>
2023-10-31 17:36:00 +0800

    本地图片映射

---
+++ src/main/java/com/closet/onlinecloset/config/MyWebAppConfigurer.java
@@ -0,0 +1,18 @@
+package com.closet.onlinecloset.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 资源映射路径
+ */
+@Configuration
+public class MyWebAppConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //外部访问路径映射到本地磁盘路径
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file:"+System.getProperty("user.dir")+"/"+"images/");
+    }
+}
